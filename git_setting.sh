8670a24d8921842d2d9ceeba17caf8d098a2aa06
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-01-10 21:28:07 +0900

    test

--- git_setting.sh
+++ git_setting.sh
@@ -2,10 +2,10 @@
 git config --global user.name akisuzuk
 git config --global user.email 122352761+akisuzuk@users.noreply.github.com
 
-# add commit push
+# add commit pushとかの流れ
 # git add .
 # git commit -m "message"
-# git push origin main
+# git push origin main（origni mainは省略可能）
 
-# clone
+# cloneの流れ
 # git clone https://github.com/akisuzuk/libft.git libft
