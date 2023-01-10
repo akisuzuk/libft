bb6ae845bdd19c230412758da7a4b8cf6793f6ad
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-01-10 18:18:39 +0900

    commit

---
+++ git_setting.sh
@@ -0,0 +1,11 @@
+# /bin/bash
+git config --global user.name akisuzuk
+git config --global user.email 122352761+akisuzuk@users.noreply.github.com
+
+# add commit push
+# git add .
+# git commit -m "message"
+# git push origin main
+
+# clone
+# git clone https://github.com/akisuzuk/libft.git libft
