c08d4cd89b9c5830fa4f7461d5070a993c336ad3
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-01-17 20:21:14 +0900

    push directory

--- ft_memset.c
+++ ft_memset.c
@@ -6,32 +6,43 @@
 /*   By: akisuzuk <akisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 17:15:47 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/01/15 17:20:04 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/01/15 17:31:26 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 void	*memset(void *b, int c, size_t len);
 
 void	*memset(void *b, int c, size_t len)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+	
 }
 
-//int	main(void)
-//{
-//	int	len;
-//
-//	len = ft_strlen("ab--cdef-gttt");
-//	printf("len=%d", len);
-//	return (0);
-//}
+int	main(void)
+{
+	char	str1[6];
+	char	str2[4];
+	int		n;
+
+	str1[0] = 'a';
+	str1[1] = 'b';
+	str1[2] = 'c';
+	str1[3] = 'd';
+	str1[4] = 'e';
+	str1[5] = '\0';
+	str2[0] = 'F';
+	str2[1] = 'G';
+	str2[2] = 'H';
+	str2[3] = '\0';
+	n = 0;
+	printf("%s\n", str1);
+	printf("%s\n", str2);
+	printf("ans = %s\n", ft_strncpy(str1, str2, n));
+	printf("%s\n", str1);
+	printf("%s\n", str2);
+	printf("------------\n");
+	return (0);
+}
