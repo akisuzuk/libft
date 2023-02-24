aa0305a7398e22219deb20d963c2dc6a1fd6f3b6
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-02-24 23:58:44 +0900

    20230224_2

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akisuzuk <akisuzuk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: akisuzuk <XXX>                             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 22:00:36 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/02/23 17:40:13 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/02/24 23:57:18 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,8 @@
 #include <math.h>
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	**free_func(char **ret, size_t size);
 char	**ft_split(char const *s, char c);
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	int					i;
-	int					int_n;
-	unsigned char		*unchar_dst;
-	const unsigned char	*cons_unchar_src;
-
-	if (n != 0)
-	{
-		i = 0;
-		unchar_dst = (unsigned char *)dst;
-		cons_unchar_src = (const unsigned char *)src;
-		int_n = (int)n;
-		while (i < int_n)
-		{
-			*unchar_dst++ = *cons_unchar_src++;
-			i++;
-		}
-		return (dst);
-	}
-	return (dst);
-}
 
 char	**free_func(char **ret, size_t size)
 {
@@ -56,7 +33,7 @@ char	**free_func(char **ret, size_t size)
 char	**ft_split(char const *s, char c)
 {
 	size_t		count;
-	char	**ret;
+	char		**ret;
 	size_t		i;
 	size_t		j;
 	size_t		head;
@@ -69,10 +46,6 @@ char	**ft_split(char const *s, char c)
 			count++;
 		i++;
 	}
-	//printf("count=%d\n", count);
-	//ret = malloc(sizeof(char *) * (len - kugiri + 1));
-	// 勘違いしてたけど外側のmallocは要素数だけハコ作ればok(マジキチのブログ参照)
-	//ret = malloc(sizeof(char *) * 7);
 	ret = malloc(sizeof(char *) * (count + 1));
 	if (!ret)
 		return (NULL);
@@ -97,10 +70,7 @@ char	**ft_split(char const *s, char c)
 		while (s[j] && s[j] != c)
 			j++;
 		ret[i] = malloc(sizeof(char) * (j - head + 1));
-		//if (!ret[i])
-		//	return (free_func(ret, i));
 		ft_memmove(ret[i], s + head, j - head);
-		// ft_memcpy(ret[i], s + head, j - head);
 		ret[i][j - head] = '\0';
 		i++;
 		j++;
@@ -137,18 +107,18 @@ size_t	count_words(char const *s, char c)
 // 	char	*splitme;
 // 	char	**tab;
 // 	int		i;
-// 
+//
 // 	splitme = strdup("--1-2--3---4----5-----42");
 // 	tab = ft_split(splitme, '-');
 // 	i = 0;
 // 	while (tab[i])
 // 		printf("ret=%s\n", tab[i++]);
-// 
+//
 // //	char	str1[] = "--1-2--3---4----5-----42";
 // //	char	str2 = '-';
 // 	//char	str1[] = " abc  def ghi  ";
 // 	//char	str2 = ' ';
-// 
+//
 // //	char	**joined;
 // //	int		i;
 // //
