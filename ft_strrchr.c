625ed799ecec5ca71eba608cfb2c866e3c250be8
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-02-25 22:31:07 +0900

    20230225_8

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: akisuzuk <XXX>                             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 11:30:57 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/02/25 22:03:04 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/02/25 22:30:04 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ char	*ft_strrchr(const char *s, int c)
 	const char	*last;
 	int			flag;
 
+	last = 0;
 	char_c = (char)c;
 	while (*s)
 	{
@@ -42,7 +43,7 @@ char	*ft_strrchr(const char *s, int c)
 		}
 		s++;
 	}
-	if (c == 0 && *s == c)
+	if (c == '\0')
 	{
 		last = s;
 		flag = 1;
@@ -61,8 +62,14 @@ char	*ft_strrchr(const char *s, int c)
 //
 //	//printf("%c\n", 't'+1);
 //	//p = strrchr(test, 0);
-//	p = ft_strrchr(test, 0);
+//	p = ft_strrchr(test, '\0');
 //	printf("ret= %s\n", p);
+//	// ↓ここのif文、pがnullの場合条件文でチェックするだけでセグフォ発動するので注意
+//	if(*p == '\0')
+//		printf("*p==EOS!\n");
+//	if(p == NULL)
+//		printf("*p==NULL!\n");
 //	return (0);
 //}
+//
 //
